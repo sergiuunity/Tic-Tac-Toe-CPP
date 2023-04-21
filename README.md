c9d036bc302011312a0fa1bd59b74efa014ced3d
Sergiu <58399256+sergiuunity@users.noreply.github.com>
2023-04-21 17:07:21 +0300

    Multiple rounds added

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 A working Tic-Tac-Toe game for 2 players made using C++.
 
-Run game.exe to play 1 round.
+Run one_round.exe to play 1 round.
+
+Run multiple_rounds.exe to play as many rounds as you want.
--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -4,22 +4,20 @@
 #include <cmath>
 #include <map>
 
-using namespace std;
-
-map<char, int> status_dictionary = { {'X',-1}, {'0', 1} };
+std::map<char, int> status_dictionary = { {'X',-1}, {'0', 1} };
 
 //displays the current sheet
 void display_sheet(char sheet[])
 {
-    cout << "   |   |" << endl;
-    cout << " " << sheet[0] << " | "<< sheet[1] << " | " << sheet[2] << endl;
-    cout << "___|___|___" << endl;
-    cout << "   |   |" << endl;
-    cout << " " << sheet[3] << " | " << sheet[4] << " | " << sheet[5] << endl;
-    cout << "___|___|___" << endl;
-    cout << "   |   |" << endl;
-    cout << " " << sheet[6] << " | " << sheet[7] << " | " << sheet[8] << endl;
-    cout << "   |   |" << endl << endl;
+    std::cout << "   |   |" << std::endl;
+    std::cout << " " << sheet[0] << " | "<< sheet[1] << " | " << sheet[2] << std::endl;
+    std::cout << "___|___|___" << std::endl;
+    std::cout << "   |   |" << std::endl;
+    std::cout << " " << sheet[3] << " | " << sheet[4] << " | " << sheet[5] << std::endl;
+    std::cout << "___|___|___" << std::endl;
+    std::cout << "   |   |" << std::endl;
+    std::cout << " " << sheet[6] << " | " << sheet[7] << " | " << sheet[8] << std::endl;
+    std::cout << "   |   |" << std::endl << std::endl;
 
 }
 
@@ -52,63 +50,132 @@ int game_status(char sheet[])
     return 0;
 }
 
+//display current score
+void display_current_score(float score_X, float score_0, bool is_final)
+{
+    std::cout << std::endl;
+    std::cout << "----------------------" << std::endl;
+    if (!is_final)
+        std::cout << "CURRENT SCORE" << std::endl;
+    else
+        std::cout << "FINAL SCORE" << std::endl;
+    std::cout << "----------------------" << std::endl;
+    std::cout << score_X << " - " << score_0 << std::endl;
+    std::cout << "----------------------" << std::endl << std::endl;
+    if (is_final)
+        if (score_X > score_0)
+            std::cout << "First Player(X) is the OVERALL WINNER!" << std::endl;
+        else
+            if (score_X < score_0)
+                std::cout << "Second Player(X) is the OVERALL WINNER!" << std::endl;
+            else
+                std::cout << "The final result is a DRAW!" << std::endl;
+}
+
 int main()
 {
-    //initial sheet
-    char sheet[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    char continue_playing[11];
 
-    display_sheet(sheet);
+    float score_X = 0;
+    float score_0 = 0;
 
-    for (int i = 0; i < 9; i++)
+    char empty_char_array[2]{};
+    char end_line[2]{ '\n' };
+
+    int round_number = 1;
+
+    do
     {
-        int move;
-        switch (i % 2)
-        {
-        //1st player turn
-        case 0:
-            cout << "First Player(X); Insert your next move...";
-            while (!(cin >> move) || sheet[move - 1] == 'X' || sheet[move - 1] == '0')
-            {
-                cout << "Insert a valid value...";
-                cin.clear();
-                cin.ignore(INT_MAX, '\n');
-            }
-            sheet[move - 1] = 'X';
-            break;
 
-        //2nd player turn
-        case 1:
-            cout << "Second Player(0); Insert your next move...";
-            while (!(cin >> move) || sheet[move - 1] == 'X' || sheet[move - 1] == '0')
-            {
-                cout << "Insert a valid value...";
-                cin.clear();
-                cin.ignore(INT_MAX, '\n');
-            }
-            sheet[move - 1] = '0';
-            break;
-        }
-        cout << endl;
+        //initial("empty") sheet
+        char sheet[10] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        std::cout << "~ROUND " << round_number << "~" << std::endl << std::endl;
 
         display_sheet(sheet);
 
-        //check if someone won
-        int crt_status = game_status(sheet);
-        if (game_status(sheet) == -1)
+
+        for (int i = 0; i < 9; i++)
         {
-            cout << "First Player(X) WON!" << endl;
-            break;
-        }
-        else
-            if (game_status(sheet) == 1)
+            int move;
+            switch (i % 2)
             {
-                cout << "Second Player(0) WON!" << endl;
+            //1st player turn
+            case 0:
+                std::cout << "First Player(X); Insert your next move...";
+                while (!(std::cin >> move) || sheet[move - 1] == 'X' || sheet[move - 1] == '0' || move > 9)
+                {
+                    std::cout << "Insert a valid value...";
+                    std::cin.clear();
+                    std::cin.ignore(INT_MAX, '\n');
+                }
+                sheet[move - 1] = 'X';
+                break;
+
+            //2nd player turn
+            case 1:
+                std::cout << "Second Player(0); Insert your next move...";
+                while (!(std::cin >> move) || sheet[move - 1] == 'X' || sheet[move - 1] == '0' || move > 9)
+                {
+                    std::cout << "Insert a valid value...";
+                    std::cin.clear();
+                    std::cin.ignore(INT_MAX, '\n');
+                }
+                sheet[move - 1] = '0';
                 break;
             }
-    }
+            std::cout << std::endl;
 
-    //check if draw
-    if (game_status(sheet) == 0)
-        cout << "The Game Is A DRAW!" << endl;
+            display_sheet(sheet);
 
+            //check if someone won
+            int crt_status = game_status(sheet);
+            if (game_status(sheet) == -1)
+            {
+                score_X += 1;
+                std::cout << "First Player(X) WON!" << std::endl;
+                break;
+            }
+            else
+                if (game_status(sheet) == 1)
+                {
+                    score_0 += 1;
+                    std::cout << "Second Player(0) WON!" << std::endl;
+                    break;
+                }
+        }
+
+
+        //check if draw
+        if (game_status(sheet) == 0)
+        {
+            std::cout << "The Game Is A DRAW!" << std::endl;
+            score_X += 0.5f;
+            score_0 += 0.5f;
+        }
+
+        display_current_score(score_X, score_0, false);
+
+
+        std::cout << "Play another round(y/n)?" << std::endl;
+        std::cin.ignore(INT_MAX, '\n');
+        std::cin.get(continue_playing, 10);
+       
+        while (strcmp(continue_playing, "y") != 0 && strcmp(continue_playing, "n") != 0 && strcmp(continue_playing, "Y") != 0 && strcmp(continue_playing, "N") != 0)
+        {
+            std::cout << "Input a valid value(y/n)...";
+            std::cin.clear();
+            std::cin.ignore(INT_MAX, '\n');
+            std::cin.get(continue_playing, 10);
+        }
+        round_number++;
+        std::cout << std::endl;
+    } while (strcmp(continue_playing, "y") == 0 || strcmp(continue_playing, "Y") == 0);
+
+
+    display_current_score(score_X, score_0, true);
+
+    system("pause");
 }
+
+//56
