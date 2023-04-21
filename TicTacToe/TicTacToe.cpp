9ab1404e7c633c2747ac162cd33c089ae810d082
Sergiu <58399256+sergiuunity@users.noreply.github.com>
2023-04-21 09:18:09 +0300

    added display functionality

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,20 +1,23 @@
-// TicTacToe.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+void display_sheet(char sheet[])
+{
+    cout << "   |   |" << endl;
+    cout << " " << sheet[0] << " | "<< sheet[1] << " | " << sheet[2] << endl;
+    cout << "___|___|___" << endl;
+    cout << "   |   |" << endl;
+    cout << " " << sheet[3] << " | " << sheet[4] << " | " << sheet[5] << endl;
+    cout << "___|___|___" << endl;
+    cout << "   |   |" << endl;
+    cout << " " << sheet[6] << " | " << sheet[7] << " | " << sheet[8] << endl;
+    cout << "   |   |" << endl;
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char sheet[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    display_sheet(sheet);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
